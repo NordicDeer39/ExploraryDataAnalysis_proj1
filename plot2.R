ed2723b7fdf124a1086ee957e7754baf451a7664
Zahra Abbasalinia <77359367+NordicDeer39@users.noreply.github.com>
2021-09-02 15:43:11 +0430

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,14 @@
+sourcefile <- "household_power_consumption.txt"
+data <- read.table(sourcefile, header = TRUE, sep = ";", stringsAsFactors = FALSE, dec = ".")
+
+# subsetting data for mentioned date
+dateddata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+## plotting
+# histogram for plot2
+
+datetime <- strptime(paste(dateddata$Date, dateddata$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+activepower <- as.numeric(dateddata$Global_active_power)
+png("plot2.png")
+plot(datetime, activepower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
