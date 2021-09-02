b3bd3379307aa9a6d30ce15c1210ba9248bb13ff
Zahra Abbasalinia <77359367+NordicDeer39@users.noreply.github.com>
2021-09-02 15:47:17 +0430

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,20 @@
+# reading data file:
+sourcefile <- "household_power_consumption.txt"
+data <- read.table(sourcefile, header = TRUE, sep = ";", stringsAsFactors = FALSE, dec = ".")
+
+# subsetting data - mentioned date:
+dateddata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+# histogram for plot3
+datetime <- strptime(paste(dateddata$Date, dateddata$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+activepower <- as.numeric(dateddata$Global_active_power)
+subMetering1 <- as.numeric(dateddata$Sub_metering_1)
+subMetering2 <- as.numeric(dateddata$Sub_metering_2)
+subMetering3 <- as.numeric(dateddata$Sub_metering_3)
+
+png("plot3.png")
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.off()
